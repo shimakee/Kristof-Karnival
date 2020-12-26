6f0d5627f63bf8d32474118f24f27a38ae00ba43
shimakee <shimakee@gmail.com>
2020-12-27 01:11:20 +0800

    Buggy grid movement applied
    now moves in increments
    bug - on collide makes it offset the position out of the grid.
    bug- changing input while still moveing offsets the position form grid.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.UI;
+
 
 public class Movement
 {
     public float Speed;
-
     public Movement(float speed)
     {
         Speed = speed;
     }
 
-   public Vector2 Calculate(float x, float y, float deltaTime)
+    Vector2 Calculate(Vector2 direction, float deltaTime)
     {
-        float horizontal = x * Speed * deltaTime;
-        float vertical = y * Speed * deltaTime;
+        float horizontal = direction.x * Speed * deltaTime;
+        float vertical = direction.y * Speed * deltaTime;
 
 
         return new Vector2(horizontal, vertical);
+    }
+
+    public void MovePosition2D(Rigidbody2D rb, Vector2 position)
+    {
+            rb.MovePosition(position);
+    }
+
+    public void AddForce2D(Rigidbody2D rb, Vector2 direction, float deltaTime)
+    {
+        rb.AddForce(Calculate(direction, deltaTime));
+    }
+
+    public void AssignVelocity2D(Rigidbody2D rb, Vector2 direction)
+    {
+        rb.velocity = direction * Speed;
+    }
+
+    public void MoveTowards2D(Rigidbody2D rb, Vector2 destination, float deltaTime)
+    {
+        rb.position = Vector2.MoveTowards(rb.position, destination, Speed * deltaTime);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,30 +9,58 @@ using UnityEngine.InputSystem.Users;
 public class Player : MonoBehaviour
 {
     [SerializeField] PlayerInput playerInput;
+    [SerializeField] float speed = 1;
 
     Animator _animator;
-    Vector2 _direction;
-    float speed = 1;
+    Rigidbody2D _rb;
+    Movement _movement;
+
+    Vector2 _direction = Vector2.zero;
+    Vector2 _desiredPosition;
 
     private void Awake()
     {
         _animator = GetComponentInChildren<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _movement = new Movement(speed);
 
+        if (_rb == null)
+            Debug.LogError("no rigidbody component attached");
         if (_animator == null)
-            Debug.Log("no animator component found in children");
+            Debug.LogError("no animator component found in children");
     }
 
     private void Start()
     {
-        playerInput.SwitchCurrentActionMap("Explore mode");
+        _desiredPosition = _rb.position;
     }
 
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        //_movement.AddForce2D(_rb, _direction, Time.deltaTime);
+        //_movement.AssignVelocity2D(_rb, _direction);
+        //_movement.MovePosition2D(_rb, _direction);
+        _movement.MoveTowards2D(_rb, _desiredPosition, Time.deltaTime);
+    }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
         _direction = ctx.ReadValue<Vector2>();
+        Debug.Log($"direction: {_direction}");
+
+        //TODO: move to separate animation handler
         _animator.SetFloat("Direction", _direction.x);
         _animator.SetFloat("Magnitude", _direction.magnitude);
+
+        if (ctx.started)
+        {
+            _desiredPosition = _rb.position + _direction;
+            Debug.Log($"Desired position: {_desiredPosition}");
+        }
     }
 
     public void OnInteract(InputAction.CallbackContext ctx)
@@ -45,10 +73,6 @@ public class Player : MonoBehaviour
         Debug.Log("Paused");
     }
 
-    private void Update()
-    {
-        transform.Translate(_direction * speed * Time.deltaTime);
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
