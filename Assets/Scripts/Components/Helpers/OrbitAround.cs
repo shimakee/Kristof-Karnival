8941e23bbb05325321954c5bdf62fe6166638ab0
shimakee <shimakee@gmail.com>
2021-01-15 21:29:13 +0800

    Created OrbitAroundScript
    Created OrbitAroundScript Useful for Camera control to circle around the player specially used together with billboarding.

---
+++ Assets/Scripts/Components/Helpers/OrbitAround.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAround : MonoBehaviour
+{
+    [SerializeField] GameObject objectToCircleAround;
+    [SerializeField] Vector3 Offset;
+    [Range(0, 360)][SerializeField] float degreesPosition;
+
+    [SerializeField] bool ZAsY;
+    //[SerializeField] float speed;
+    [SerializeField] float radius;
+
+    Vector3 _newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _newPosition.x = objectToCircleAround.transform.position.x;
+        _newPosition.y = objectToCircleAround.transform.position.y;
+        _newPosition.z = objectToCircleAround.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_time += Time.deltaTime * speed;
+    }
+
+    private void LateUpdate()
+    {
+        _newPosition = objectToCircleAround.transform.position;
+
+        _newPosition.x = _newPosition.x + Mathf.Cos(degreesPosition * Mathf.Deg2Rad) * radius;
+        if (ZAsY)
+            _newPosition.z = _newPosition.z + Mathf.Sin(degreesPosition * Mathf.Deg2Rad) * radius;
+        else
+            _newPosition.y = _newPosition.y + Mathf.Sin(degreesPosition * Mathf.Deg2Rad) * radius;
+
+        transform.position = _newPosition + Offset;
+    }
+}
