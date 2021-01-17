eb64ef4f7b84dbb3e313145757b6695a42c8bb5d
shimakee <shimakee@gmail.com>
2021-01-18 00:13:21 +0800

    Created stairs and dynamic navmeshObstacle

--- Assets/Scripts/Components/Helpers/OrbitAround.cs
+++ Assets/Scripts/Components/Helpers/OrbitAround.cs
@@ -7,13 +7,15 @@ public class OrbitAround : MonoBehaviour
     [SerializeField] GameObject objectToCircleAround;
     [SerializeField] Vector3 Offset;
     [Range(0, 360)][SerializeField] float degreesPosition;
+    [SerializeField] bool automatic;
+    [SerializeField] float speed = 2;
 
     [SerializeField] bool ZAsY;
     //[SerializeField] float speed;
     [SerializeField] float radius;
 
     Vector3 _newPosition;
-
+    float _time;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,19 +27,30 @@ public class OrbitAround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //_time += Time.deltaTime * speed;
+        _time += Time.deltaTime * speed;
     }
 
     private void LateUpdate()
     {
         _newPosition = objectToCircleAround.transform.position;
 
-        _newPosition.x = _newPosition.x + Mathf.Cos(degreesPosition * Mathf.Deg2Rad) * radius;
-        if (ZAsY)
-            _newPosition.z = _newPosition.z + Mathf.Sin(degreesPosition * Mathf.Deg2Rad) * radius;
+        if (!automatic)
+        {
+            _newPosition.x = _newPosition.x + Mathf.Cos(degreesPosition * Mathf.Deg2Rad) * radius;
+            if (ZAsY)
+                _newPosition.z = _newPosition.z + Mathf.Sin(degreesPosition * Mathf.Deg2Rad) * radius;
+            else
+                _newPosition.y = _newPosition.y + Mathf.Sin(degreesPosition * Mathf.Deg2Rad) * radius;
+        }
         else
-            _newPosition.y = _newPosition.y + Mathf.Sin(degreesPosition * Mathf.Deg2Rad) * radius;
+        {
+            _newPosition.x = _newPosition.x + Mathf.Cos(_time * Mathf.Deg2Rad) * radius;
+            if (ZAsY)
+                _newPosition.z = _newPosition.z + Mathf.Sin(_time * Mathf.Deg2Rad) * radius;
+            else
+                _newPosition.y = _newPosition.y + Mathf.Sin(_time * Mathf.Deg2Rad) * radius;
+        }
 
-        transform.position = _newPosition + Offset;
+            transform.position = _newPosition + Offset;
     }
 }
