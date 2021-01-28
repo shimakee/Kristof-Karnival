fea09e7d956f50392f5c12723b6fc69edba82b97
shimakee <shimakee@gmail.com>
2021-01-29 00:11:27 +0800

    Created path finding along a path
    created a path finding that travels along a path using array of gameObjects as waypoints.
    
    Bug to fix: it travels along the path no matter how far away it is as long as it stays in the same trajectory. need to clamp length to find closest path.

--- Assets/Scripts/Components/Steering/SteeringBehaviour.cs
+++ Assets/Scripts/Components/Steering/SteeringBehaviour.cs
@@ -13,6 +13,7 @@ public class SteeringBehaviour : MonoBehaviour
 
     [SerializeField] GameObject[] path;
     [SerializeField] float pathRadius;
+    Vector3[] pathLocations;
 
     IDirectionMoverComponent _mover;
     Vector3 _direction;
@@ -24,8 +25,9 @@ public class SteeringBehaviour : MonoBehaviour
 
     private void Update()
     {
-        _direction += Seek(target.transform.position, _mover.CurrentPosition) * Time.deltaTime;
-        _direction += FollowPath(path[0].transform.position, path[1].transform.position);
+        _direction += Seek(target.transform.position) * Time.deltaTime;
+        //_direction += FollowPath(path[0].transform.position, path[1].transform.position);
+        _direction += FollowAlongPaths(path) * Time.deltaTime;
         _direction = Arriving(_direction, target.transform.position, arrivingDistance);
 
         _mover.MoveDirection(_direction);
@@ -37,9 +39,9 @@ public class SteeringBehaviour : MonoBehaviour
     }
 
     #region Seek
-    private Vector3 Seek(Vector3 targetPosition, Vector3 currentPosition)
+    private Vector3 Seek(Vector3 targetPosition)
     {
-        var desired = targetPosition - currentPosition;
+        var desired = targetPosition - _mover.CurrentPosition;
             desired = desired.normalized * maxTravelSpeed;
 
         var steering = desired - _direction;
@@ -57,6 +59,44 @@ public class SteeringBehaviour : MonoBehaviour
 
     }
 
+    private Vector3 FollowAlongPaths(GameObject[] gameObjects)
+    {
+        pathLocations = new Vector3[gameObjects.Length];
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            pathLocations[i] = gameObjects[i].transform.position;
+        }
+
+        int shortestDistance = 0;
+        float distance = 0f;
+        for (int i = 0; i < pathLocations.Length; i++)
+        {
+            var firstPos = pathLocations[i];
+            var secondPos = (i >= pathLocations.Length -1) ? pathLocations[0] : pathLocations[i + 1];
+
+            Vector3 path = secondPos - firstPos;
+            Vector3 playerPositionRelativeToPath = _mover.CurrentPosition - firstPos;
+            //Vector3 currentPositionAlongPath = path.normalized * Vector3.Dot(playerPositionRelativeToPath, path.normalized);
+            Vector3 playerFuturePosition = playerPositionRelativeToPath + _direction;
+            Vector3 futurePositionAlongPath = path.normalized * Vector3.Dot(playerFuturePosition, path.normalized);
+
+            var distanceFromPath = Vector3.Distance(futurePositionAlongPath, playerFuturePosition); ;
+            if (i == 0)
+                distance = distanceFromPath;
+            else if ( distance > distanceFromPath)
+            {
+                shortestDistance = i;
+                distance = distanceFromPath;
+            }
+        }
+
+        Vector3 pathOne = pathLocations[shortestDistance];
+        Vector3 pathTwo = (shortestDistance >= pathLocations.Length - 1) ? pathLocations[0] : pathLocations[shortestDistance + 1];
+
+        return FollowPath(pathOne, pathTwo);
+    }
+
     private Vector3 FollowPath(Vector3 firstPos, Vector3 secondPos)
     {
         Vector3 path = secondPos - firstPos;
@@ -68,10 +108,10 @@ public class SteeringBehaviour : MonoBehaviour
         float distance = Vector3.Distance(futurePositionAlongPath, playerFuturePosition);
 
         Debug.DrawLine(firstPos, futurePositionAlongPath + firstPos);
-        Debug.DrawLine(futurePositionAlongPath, playerFuturePosition);
+        Debug.DrawLine(futurePositionAlongPath + firstPos, playerFuturePosition + firstPos);
 
         if (distance > pathRadius)
-            return Seek(futurePositionAlongPath + firstPos, _mover.CurrentPosition);
+            return Seek(futurePositionAlongPath + firstPos);
         else
             return Vector3.zero;
     }
