87600ed673b6d4dd0113f42e617592b4f4f4baf7
shimakee <shimakee@gmail.com>
2021-02-04 21:24:11 +0800

    Created state machine using script
    Created AiStateMachineComponent as state machine.

---
+++ Assets/Scripts/Components/AI/AIStateMachineComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(IDirectionMoverComponent), typeof(IFieldOfView))]
+public class AIStateMachineComponent : MonoBehaviour
+{
+    public int state = 0;
+    public float speed = 5;
+    public float steering = 5;
+    //public ISteeringBehaviour[] behaviours;
+    private IDirectionMoverComponent mover;
+    private IFieldOfView fieldOfView;
+    private ISteeringBehaviour seek;
+    Vector3 _direction = Vector3.zero;
+
+    private void Awake()
+    {
+        fieldOfView = GetComponent<IFieldOfView>();
+        mover = GetComponent<IDirectionMoverComponent>();
+        seek = new SeekBehaviour(speed, steering);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == 1)
+        {
+            _direction += seek.CalculateSteeringForce(mover, fieldOfView) * Time.deltaTime;
+        }
+        mover.MoveDirection(_direction);
+    }
+}
